d9452cb3e9e817289117333f3dc663a195ef7fab
Asher9889 <saurabhkushwaha9889@gmail.com>
2024-04-13 15:12:05 +0530

    local storage added

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const nightButton = document.querySelector(".day-night");
 const banner = document.querySelector(".banner")
 
+// localStorage.setItem("theme", "light")
 
 nightButton.addEventListener("click", ()=>{
   banner.classList.toggle("night-mode");
+  if(localStorage.getItem("theme") == "light"){
+    localStorage.setItem("theme", "dark")
+  }else{
+    localStorage.setItem("theme", "light")
+  }
 })
 
 const typeEffect = new Typed("#name", {
@@ -12,3 +18,11 @@ const typeEffect = new Typed("#name", {
   loop: true,
   backSpeed: 100,
 })
+
+
+
+if(localStorage.getItem("theme") == "light"){
+  banner.classList.remove("night-mode")
+}else if(localStorage.getItem("theme") != "light"){
+  banner.classList.add("night-mode")
+}
